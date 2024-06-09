e0c7a5235493b8985670d11ea4ab6f3c7a06153b
Civil Basumatary <u21cse1015@cit.ac.in>
2024-06-09 06:14:22 -0700

    Delete project directory

--- project/src/main/java/com/summerinternship/project/repository/OfficeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.summerinternship.project.repository;
-
-import com.summerinternship.project.model.Office;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OfficeRepository extends CrudRepository<Office, Long> {
-}
